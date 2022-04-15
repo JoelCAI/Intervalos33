57796bbdbe33e1acc92b44d4cdbc5458cdfefebc
JoelCAI <joelargentina12345@gmail.com>
2022-04-14 23:12:02 -0300

    Agregar archivos de proyecto.

---
+++ Intervalos33/Sistema.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intervalos33
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+            Validador.Bienvenida();
+            Console.WriteLine("\n Recuerde seguir las instrucciones. Presione cualquier tecla para continuar");
+
+
+            DateTime fechaUno = Validador.ValidarFechaIngresada("\n Ingrese una fecha y sabrá cuantos dias faltan " +
+                                                                "para fin de año.");
+
+            DateTime fechaDos = new DateTime(fechaUno.Year,12,31);
+
+            TimeSpan intervaloTiempo = fechaDos.Subtract(fechaUno);
+
+
+            Console.Clear();
+            Console.WriteLine("\n Faltan: " + intervaloTiempo.Days / 1 + " días" +
+                              "\n Para el 31 de Diciembre del " +fechaUno.Year + ".");
+
+            Validador.Despedida();
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
